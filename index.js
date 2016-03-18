18bd8af06b38136ce83890e0c82fbcf634d811d5
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-03-18 18:43:51 +0200

    start reworking develop checks

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ function List ( config ) {
     // sanitize
     config = config || {};
 
+    console.assert(typeof this === 'object', 'must be constructed via new');
+    
     if ( DEVELOP ) {
         if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
         // init parameters checks
